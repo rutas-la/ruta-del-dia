eafe1012d98e1e9542a60fc640a9d947ce9572fa
rutas-la <loretoma1970@gmail.com>
2026-07-11 13:55:41 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "ruta-del-dia-v3";
+const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  // Solo cacheamos el shell propio; las APIs (Nominatim, OSRM, tiles) siempre van a la red.
+  if (url.origin === self.location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(hit =>
+        hit ||
+        fetch(e.request).then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+          return res;
+        })
+      )
+    );
+  }
+});
