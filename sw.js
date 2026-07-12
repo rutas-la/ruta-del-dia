7db403c2b8a6e3a85cb706a588007a92291fd421
rutas-la <loretoma1970@gmail.com>
2026-07-11 21:02:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ruta-del-dia-v10";
+const CACHE = "ruta-del-dia-v11";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
